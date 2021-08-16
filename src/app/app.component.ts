424c000055194e7dd1eaa8badb09150135957bf9
Helena Nakamura <helena.reiko@gmail.com>
2021-08-16 17:58:15 -0300

    arquivos do projeto angular- autenticacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from './servicos/http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'autenticacao';
+
+  constructor(private router: Router,
+             private httpService:HttpService) { }
+
+  sair()  {
+    delete localStorage['token'];
+    this.router.navigate(['/login']);
+  }
+  
+  autenticado(){
+    return this.httpService.autenticado();
+  }
 }
