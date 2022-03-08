184d786679b31e303cb37213a44c2b056f8495b5
Helena Nakamura <helena.reiko@gmail.com>
2022-03-08 11:09:08 -0300

    acertos

--- src/app/filtros/data.pipe.ts
+++ src/app/filtros/data.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DataPipe implements PipeTransform {
 
   transform(value: string, ...args: string[]): Date {
-    return new Date(value.replace(' ', 'T'));
+    return new Date(value.replace(' ', 'T')); // safari so reconhece se tiver T
   }
   /*  outra opção
   transform(value: string, ...args: string[]): Date {
